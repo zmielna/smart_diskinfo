97c89aff7732387364dc5c23dcaeb5f7a68520ec
zmielna <zmielna@users.noreply.github.com>
2015-11-06 09:42:57 +0000

    Comments added

--- check_mk_smart_freebsd.sh
+++ check_mk_smart_freebsd.sh
@@ -5,6 +5,15 @@
 # This is Check_mk local check script for FreeBSD based NAS like NAS4FREE
 # Drop a copy to /usr/lib/check_mk_agent/local/
 # then "telnet nas4free.domain.com 6556" from your OMD server 
+#    SMART 5 \u2013 Reallocated_Sector_Count.
+#    SMART 187 \u2013 Reported_Uncorrectable_Errors.
+#    SMART 188 \u2013 Command_Timeout.
+#    SMART 197 \u2013 Current_Pending_Sector_Count.
+#    SMART 198 \u2013 Offline_Uncorrectable.
+# So we care mostly about 187 but if your disk doesn't return this value use 198 or other
+#
+# For linux change /dev/ada? to /dev/sd?
+
 for i in `ls /dev/ada?`;
         do
                 ERRORS=`smartctl -a $i|egrep '^187'|awk '{print $NF}'`
